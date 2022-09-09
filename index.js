494947476dcb563e4307d3d63ecc3ab37857f52e
Alexandru Cuciureanu <alexandru.cuciureanu@gmail.com>
2022-09-09 22:38:49 +0300

    feat: 🎸 command line options

--- index.js
+++ index.js
@@ -5,22 +5,16 @@ import cdnjs from './services/cdnjs.service.js';
 
 const program = new Command();
 
+program.name('ppfang').description('A tool which helps identifying client-side prototype polluting libraries');
+
 program
-    .name('ppfang')
-    .description('A tool which helps identifying client-side prototype polluting libraries');
-
-program.command('check')
-    .option('-c, --concurrency [concurrency]', 'the level of concurrency', '10')
+    .command('check')
+    .description('Verifies the latest libraries from cdnjs.com')
+    .option('-c, --concurrency <concurrency>', 'concurrency level', '10')
+    .action((options) => {
+        const concurrency = Number.parseInt(options.concurrency);
+        (concurrency) ? cdnjs.probeAll(concurrency) : cdnjs.probeAll(10)
+    })
     .addHelpText('afterAll', '\nThanks for using this tool :)');
-
-const opts = program.opts();
-
-const concurrency = Number.parseInt(opts.concurrency);
-
-if (concurrency > 0) {
-    cdnjs.probeAll(concurrency)
-} else {
-    cdnjs.probeAll();
-}
 
 program.parse();

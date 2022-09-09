5c1938e75ab890b9293b7cf431ea70d55f9e95ca
Alexandru Cuciureanu <alexandru.cuciureanu@gmail.com>
2022-09-09 18:01:08 +0300

    added command line tooling for ppfang

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
 import cdnjs from './services/cdnjs.service.js';
 
-(async () => await cdnjs.probeAll())();
+const program = new Command();
+
+program
+    .description('A tool which helps identifying client-side prototype polluting libraries')
+    .option('-c, --cdnjs', 'Test the latest libraries from cdnjs.com');
+
+program.parse();
+
+const options = program.opts();
+
+if (options.cdnjs) {
+    cdnjs.probeAll();
+} else {
+    program.help();
+}
+
+program.parse();

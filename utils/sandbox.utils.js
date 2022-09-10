3d221dceb202c696778a47f5663079acae9f2741
Alexandru Cuciureanu <alexandru.cuciureanu@gmail.com>
2022-09-10 20:06:30 +0300

    ci: 🎡 added .github folder in .npmignore

--- utils/sandbox.utils.js
+++ utils/sandbox.utils.js
@@ -1,4 +1,7 @@
 import fs from 'node:fs';
 import path from 'node:path';
 
-export const sandboxHtml = fs.readFileSync(path.join(process.cwd(), '/sandbox/index.html'), { encoding: 'utf-8' });
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+export const sandboxHtml = fs.readFileSync(path.resolve(path.join(__dirname, '../sandbox/index.html')), {
+    encoding: 'utf-8',
+});

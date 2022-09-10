1f555e5b0e55f4abbab7b516a55ff64dcc971da6
Alexandru Cuciureanu <alexandru.cuciureanu@gmail.com>
2022-09-10 09:25:09 +0300

    refactor: 💡 added afterAll description
    added afterAll description for the command-line menu

--- index.js
+++ index.js
@@ -5,7 +5,25 @@ import cdnjs from './services/cdnjs.service.js';
 
 const program = new Command();
 
-program.name('ppfang').description('A tool which helps identifying client-side prototype polluting libraries');
+program.addHelpText(
+    'afterAll',
+    `
+
+Examples:
+
+  ppfang check
+
+  ppfang check -c 50
+
+  ppfang --help
+
+`
+);
+
+program
+    .name('ppfang')
+    .usage('[command] [option]')
+    .description('A tool which helps identifying client-side prototype polluting libraries');
 
 program
     .command('check')
@@ -13,8 +31,7 @@ program
     .option('-c, --concurrency <concurrency>', 'concurrency level', '10')
     .action((options) => {
         const concurrency = Number.parseInt(options.concurrency);
-        (concurrency) ? cdnjs.probeAll(concurrency) : cdnjs.probeAll(10)
-    })
-    .addHelpText('afterAll', '\nThanks for using this tool :)');
+        concurrency ? cdnjs.probeAll(concurrency) : cdnjs.probeAll(10);
+    });
 
 program.parse();

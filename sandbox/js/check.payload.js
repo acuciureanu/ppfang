3fffdaa8666b81e1484ffd263ba4bf675931ef16
Alexandru Cuciureanu <alexandru.cuciureanu@gmail.com>
2023-02-08 21:57:20 +0200

    Revert "feat(enhanced the prototype pollution detection mecanism): better checking for prototype pollution"
    This reverts commit 284fbb169c543b0858f757137d521bc75a80c358.

--- sandbox/js/check.payload.js
+++ sandbox/js/check.payload.js
@@ -7,41 +7,12 @@ const prototypesPropertiesReducer = (acc, type) => ({
 
 const prototypePropertyNames = types.reduce(prototypesPropertiesReducer, {});
 
-// This function checks if a prototype property is polluted by changing its value
-// and checking if the value has changed. It returns a boolean indicating if the
-// property is polluted.
-const checkIfPrototypePropertyIsPolluted = (object, prop) => {
-  // Get the original value of the prototype property
-  const originalValue = Object.getOwnPropertyDescriptor(object, prop);
-  try {
-    // Change the value of the prototype property
-    object[prop] = 'polluted';
-    // If the value of the prototype property is 'polluted', it is polluted
-    if (object[prop] === 'polluted') {
-      return true;
-    }
-  } catch (e) {
-    // The property is read-only, so it is not polluted.
-    return false;
-  } finally {
-    // Restore the original value of the prototype property
-    Object.defineProperty(object, prop, originalValue);
-  }
-  return false;
-};
-
-// This function probes for polluted prototype properties and returns an array
-// of the names of the polluted properties.
 const probe = () =>
     Object.keys(prototypePropertyNames).reduce((acc, key) => {
         for (let propKey of prototypePropertyNames[key]) {
-            // Get the prototype property using eval()
             const payload = `${key}.prototype.${propKey}`;
             try {
-                const prop = eval(payload);
-                // If the prototype property is a function and its prototype is polluted,
-                // add its name to the result array.
-                if (typeof prop === 'function' && checkIfPrototypePropertyIsPolluted(prop.prototype, propKey)) {
+                if (typeof eval(payload) === 'function' && eval(payload).call() === window) {
                     acc.push(payload);
                 }
             } catch (e) {
